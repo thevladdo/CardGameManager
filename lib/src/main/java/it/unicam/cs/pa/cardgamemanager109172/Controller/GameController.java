da2196fee5705b885b174575d46337437d5df25a
Vlad Dogariu <vlad.dogariu@studenti.unicam.it>
2022-01-21 23:35:39 +0100

    Update of .fxml file and button association
    Expansion of GameController class and creation of Main Application class

--- lib/src/main/java/it/unicam/cs/pa/cardgamemanager109172/Controller/GameController.java
+++ lib/src/main/java/it/unicam/cs/pa/cardgamemanager109172/Controller/GameController.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import java.util.Optional;
 
 public class GameController{
 
@@ -16,14 +21,61 @@ public class GameController{
     private RadioButton scala;
     @FXML
     private RadioButton rubamazzetto;
+    @FXML
+    private Pane pane;
 
     Alert a = new Alert(Alert.AlertType.WARNING);
+    public static String playerName;
+    Stage stage;
+    private int counter;
 
-    public void selectGame(ActionEvent actionEvent) {
-        if(poker.isSelected() || scala.isSelected())
-            a.setContentText("Gioco disponibile nel prossimo aggiornamento");
-        if(rubamazzetto.isSelected() && startGame.isArmed()){
-
+    public void startGame(ActionEvent event){
+        if (counter<1){
+            stage = (Stage) pane.getScene().getWindow();
+            a.initModality(Modality.APPLICATION_MODAL);
+            a.initOwner(stage);
+            counter++;
+        }
+        if ((poker.isSelected() || scala.isSelected())) {
+            a.getDialogPane().setContentText("Ci scusiamo per l'inconveniente :(");
+            a.getDialogPane().setHeaderText("Gioco disponibile nel prossimo aggiornamento");
+            a.showAndWait();
+            counter++;
+        } else if (rubamazzetto.isSelected()){
+            Stage stage = (Stage) pane.getScene().getWindow();
+            TextInputDialog dialog = new TextInputDialog("walter");
+            dialog.setTitle("UNICAM CARD GAME");
+            dialog.setHeaderText("Ciao, inserisci i seguenti dati per continuare");
+            dialog.setContentText("Il mio nome è");
+            Optional<String> result = dialog.showAndWait();
+            result.ifPresent(name -> playerName = name);
+            stage.close();
         }
     }
+
+
+    public void selectPoker(ActionEvent event){
+        if(scala.isSelected() || rubamazzetto.isSelected()){
+            scala.setSelected(false);
+            rubamazzetto.setSelected(false);
+            poker.setSelected(true);
+        }
+    }
+
+    public void selectScala(ActionEvent event){
+        if(poker.isSelected() || rubamazzetto.isSelected()){
+            poker.setSelected(false);
+            rubamazzetto.setSelected(false);
+            scala.setSelected(true);
+        }
+    }
+
+    public void selectRubamazzetto(ActionEvent event){
+        if(scala.isSelected() || poker.isSelected()){
+            scala.setSelected(false);
+            poker.setSelected(false);
+            rubamazzetto.setSelected(true);
+        }
+    }
+
 }
--- lib/src/main/java/it/unicam/cs/pa/cardgamemanager109172/Main.java
+++ lib/src/main/java/it/unicam/cs/pa/cardgamemanager109172/Main.java
@@ -1,10 +1,30 @@
 package it.unicam.cs.pa.cardgamemanager109172;
 
-public class Main{
+import it.unicam.cs.pa.cardgamemanager109172.Controller.GameController;
+import it.unicam.cs.pa.cardgamemanager109172.Model.Game.Rubamazzetto;
+import it.unicam.cs.pa.cardgamemanager109172.View.GameSelector;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Main extends Application {
 
-
+    @Override
+    public void start(Stage primaryStage){
+        GameSelector selector = new GameSelector();
+        selector.start(primaryStage);
     }
 
+    public static void main(String[] args) {
+        launch(args);
+        Scanner user = new Scanner(System.in);
+        String playerName = GameController.playerName.toUpperCase();
+        println("CIAO "+playerName+" :)\nBENVENUTO NEL GIOCO !");
+        Rubamazzetto rubamazzetto = new Rubamazzetto(playerName);
+    }
+
+    private static void println(String s){
+        System.out.println(s);
+    }
 }
+
